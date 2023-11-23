bca30c040ede9f8b8dd10441365fea933d30872e
NiharMondal <developernihar@gmail.com>
2023-11-23 13:14:42 +0600

    vercel file customised

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,11 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+//root route
 app.use("/", (req: Request, res: Response) => {
 	res.json({ success: true, message: "Welcome to the home page" });
 });
+
 //use routes
 app.use("/api/users", userRouter);
 
