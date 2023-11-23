f5c2232d59bc03346ead2e923a4a86977c29877e
NiharMondal <developernihar@gmail.com>
2023-11-23 13:06:40 +0600

    root route added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 //import routes
 
@@ -8,6 +8,9 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/", (req: Request, res: Response) => {
+	res.json({ success: true, message: "Welcome to the home page" });
+});
 //use routes
 app.use("/api/users", userRouter);
 
